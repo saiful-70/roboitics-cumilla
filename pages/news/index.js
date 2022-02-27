e99ccaa5c0c4e0944df362a86cb9477818f723ec
Saiful Islam <saiful70.me@gmail.com>
2022-02-28 00:16:09 +0600

    site is under construction and big changes will happen

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Dialog, Transition } from "@headlessui/react";
 
 import Footer from "../../components/Footer/Footer";
 import Nav from "../../components/Nav/Nav";
