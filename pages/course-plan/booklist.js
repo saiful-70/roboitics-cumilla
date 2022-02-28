5a2f1b4206001c3aff986570039ffe76610774e9
Saiful Islam <saiful70.me@gmail.com>
2022-02-28 22:19:29 +0600

    file organize

--- pages/course-plan/booklist.js
+++ pages/course-plan/booklist.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaRegDotCircle } from "react-icons/fa";
 
 import styles from "../../styles/Course.module.css";
 
@@ -20,7 +21,137 @@ const Session = () => {
           <Sidebar />
 
           <div className=" w-full lg:w-5/6">
-            <Card />
+            <div className="">
+              <p className="mt-2 mb-4 rounded-full border-2 border-solid bg-sky-900 px-9  py-3 text-3xl font-semibold leading-8 tracking-tight  text-white sm:text-4xl">
+                Book List
+              </p>
+            </div>
+
+            <div className="flex">
+              <div className="border-2 bg-sky-900  px-4 py-5">
+                <FaRegDotCircle style={{ color: "white", fontSize: "30px" }} />
+              </div>
+              <div className="flex space-x-4 border-2 bg-white px-6 py-5">
+                <p className="inline text-lg font-bold text-black ">
+                  BOOK NAME |
+                </p>{" "}
+                <p className="inline text-lg font-bold text-gray-600 ">
+                  Writer Name
+                </p>
+                <button className="rounded-full bg-green-500 px-5 font-bold text-white hover:bg-green-700">
+                  {" "}
+                  Visit{" "}
+                </button>
+              </div>
+            </div>
+
+            <div className="flex">
+              <div className="border-2 bg-sky-900  px-4 py-5">
+                <FaRegDotCircle style={{ color: "white", fontSize: "30px" }} />
+              </div>
+              <div className="flex space-x-4 border-2 bg-white px-6 py-5">
+                <p className="inline text-lg font-bold text-black ">
+                  BOOK NAME |
+                </p>{" "}
+                <p className="inline text-lg font-bold text-gray-600 ">
+                  Writer Name
+                </p>
+                <button className="rounded-full bg-green-500 px-5 font-bold text-white hover:bg-green-700">
+                  {" "}
+                  Visit{" "}
+                </button>
+              </div>
+            </div>
+
+            <div className="flex">
+              <div className="border-2 bg-sky-900  px-4 py-5">
+                <FaRegDotCircle style={{ color: "white", fontSize: "30px" }} />
+              </div>
+              <div className="flex space-x-4 border-2 bg-white px-6 py-5">
+                <p className="inline text-lg font-bold text-black ">
+                  BOOK NAME |
+                </p>{" "}
+                <p className="inline text-lg font-bold text-gray-600 ">
+                  Writer Name
+                </p>
+                <button className="rounded-full bg-green-500 px-5 font-bold text-white hover:bg-green-700">
+                  {" "}
+                  Visit{" "}
+                </button>
+              </div>
+            </div>
+
+            <div className="flex">
+              <div className="border-2 bg-sky-900  px-4 py-5">
+                <FaRegDotCircle style={{ color: "white", fontSize: "30px" }} />
+              </div>
+              <div className="flex space-x-4 border-2 bg-white px-6 py-5">
+                <p className="inline text-lg font-bold text-black ">
+                  BOOK NAME |
+                </p>{" "}
+                <p className="inline text-lg font-bold text-gray-600 ">
+                  Writer Name
+                </p>
+                <button className="rounded-full bg-green-500 px-5 font-bold text-white hover:bg-green-700">
+                  {" "}
+                  Visit{" "}
+                </button>
+              </div>
+            </div>
+
+            <div className="flex">
+              <div className="border-2 bg-sky-900  px-4 py-5">
+                <FaRegDotCircle style={{ color: "white", fontSize: "30px" }} />
+              </div>
+              <div className="flex space-x-4 border-2 bg-white px-6 py-5">
+                <p className="inline text-lg font-bold text-black ">
+                  BOOK NAME |
+                </p>{" "}
+                <p className="inline text-lg font-bold text-gray-600 ">
+                  Writer Name
+                </p>
+                <button className="rounded-full bg-green-500 px-5 font-bold text-white hover:bg-green-700">
+                  {" "}
+                  Visit{" "}
+                </button>
+              </div>
+            </div>
+
+            <div className="flex">
+              <div className="border-2 bg-sky-900  px-4 py-5">
+                <FaRegDotCircle style={{ color: "white", fontSize: "30px" }} />
+              </div>
+              <div className="flex space-x-4 border-2 bg-white px-6 py-5">
+                <p className="inline text-lg font-bold text-black ">
+                  BOOK NAME |
+                </p>{" "}
+                <p className="inline text-lg font-bold text-gray-600 ">
+                  Writer Name
+                </p>
+                <button className="rounded-full bg-green-500 px-5 font-bold text-white hover:bg-green-700">
+                  {" "}
+                  Visit{" "}
+                </button>
+              </div>
+            </div>
+
+            <div className="flex">
+              <div className="border-2 bg-sky-900  px-4 py-5">
+                <FaRegDotCircle style={{ color: "white", fontSize: "30px" }} />
+              </div>
+              <div className="flex space-x-4 border-2 bg-white px-6 py-5">
+                <p className="inline text-lg font-bold text-black ">
+                  BOOK NAME |
+                </p>{" "}
+                <p className="inline text-lg font-bold text-gray-600 ">
+                  Writer Name
+                </p>
+                <button className="rounded-full bg-green-500 px-5 font-bold text-white hover:bg-green-700">
+                  {" "}
+                  Visit{" "}
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </main>
--- pages/course-plan/training-session/index.js
+++ pages/course-plan/training-session/index.js
@@ -1,8 +1,7 @@
 import React from "react";
-import Footer from "../../components/Footer/Footer";
-import Nav from "../../components/Nav/Nav";
-import Sidebar from "../../components/Common/CourseMenu/Sidebar";
-import Card from "../../components/Home/Card";
+import Footer from "../../../components/Footer/Footer";
+import Nav from "../../../components/Nav/Nav";
+import Sidebar from "../../../components/Common/CourseMenu/Sidebar";
 
 const Session = () => {
   return (
@@ -17,7 +16,10 @@ const Session = () => {
           <Sidebar />
 
           <div className=" w-full bg-gray-200 lg:w-5/6">
-            <Card />
+            {/* you will work from here */}
+            <h3 className="mx-auto mb-5 w-max border-b-2 border-gray-400 px-10 py-1 text-center text-3xl text-gray-700">
+              School Lists
+            </h3>
           </div>
         </div>
       </main>
